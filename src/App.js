45b887dcc02ba75b67918a0da60e02162ef9a827
Mikey Schoonmaker <mcschoo@clemson.edu>
2024-04-24 11:36:29 -0400

    Working to make the code more portable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import { useState } from 'react';
 
 // Template function for the individual board tiles
-function Square() {
+function Square({value}) {
+  return <button className='square'>{value}</button>;
+  /*
   // Declare const to use as opposed to previous argument. Value begins equal to null
   const [value, setValue] = useState(null);
 
@@ -20,28 +22,34 @@ function Square() {
       {value}
     </button>
   )
+  */
 }
 
 // Default function ("main") that creates our board
 export default function Board() {
+
+  // Declare a const to create array of 9 nulls mapping to the 9 squares
+  cosnt [squares, setSquares] = useState(Array(9).fill(null));
+
+  // Creating the board - each squre recieves a "value" that will be either X, O, or null
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>       
       
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </>
   );
