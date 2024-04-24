21f3c4da92f9d864eb39ad6d91a5b9ad51954724
Mikey Schoonmaker <mcschoo@clemson.edu>
2024-04-24 15:34:25 -0400

    Implement a way to determine a winner

--- src/App.js
+++ src/App.js
@@ -70,3 +70,28 @@ export default function Board() {
     </>
   );
 }
+
+// Helper function to determine if game is won
+function calculateWinner(squares) {
+
+  // 2D array storing possible win conditions
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+  for(let i = 0; i < lines.length; i++)
+  {
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+      return squares[a]
+    }
+  }
+  return null;
+}
